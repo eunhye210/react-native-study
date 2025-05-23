737079f138505f14020f5b0d7547fbfbffe8530d
Eunhye210 <tkrhd0210@gmail.com>
2025-05-23 10:31:46 +0900

    constants setting

---
+++ src/components/Text.tsx
@@ -0,0 +1,139 @@
+import React from "react";
+import {
+  Text as RNText,
+  TextProps as RNTextProps,
+  StyleSheet,
+} from "react-native";
+import { theme } from "src/constants/theme";
+
+interface Props extends RNTextProps {
+  h1?: boolean;
+  h2?: boolean;
+  h3?: boolean;
+  title?: boolean;
+  body?: boolean;
+  caption?: boolean;
+  small?: boolean;
+  size?: number;
+  transform?: "none" | "capitalize" | "uppercase" | "lowercase";
+  align?: "auto" | "left" | "right" | "center" | "justify";
+  weight?:
+    | "normal"
+    | "bold"
+    | "100"
+    | "200"
+    | "300"
+    | "400"
+    | "500"
+    | "600"
+    | "700"
+    | "800"
+    | "900";
+  spacing?: number;
+  height?: number;
+  regular?: boolean;
+  bold?: boolean;
+  semibold?: boolean;
+  medium?: boolean;
+  light?: boolean;
+  center?: boolean;
+  right?: boolean;
+  color?: string;
+  children?: React.ReactNode;
+  style?: any;
+}
+
+export default function Text({
+  h1,
+  h2,
+  h3,
+  title,
+  body,
+  caption,
+  small,
+  size,
+  transform,
+  align,
+  height,
+  spacing,
+  weight,
+  regular,
+  bold,
+  semibold,
+  medium,
+  light,
+  center,
+  right,
+  color,
+  style,
+  children,
+  ...rest
+}: Props) {
+  const textStyles = [
+    styles.text,
+    h1 && styles.h1,
+    h2 && styles.h2,
+    h3 && styles.h3,
+    title && styles.title,
+    body && styles.body,
+    caption && styles.caption,
+    small && styles.small,
+    size && { fontSize: size },
+    transform && { textTransform: transform },
+    align && { textAlign: align },
+    height && { lineHeight: height },
+    spacing && { letterSpacing: spacing },
+    weight && { fontWeight: weight },
+    regular && styles.regular,
+    bold && styles.bold,
+    semibold && styles.semibold,
+    medium && styles.medium,
+    light && styles.light,
+    center && styles.center,
+    right && styles.right,
+    color && styles[color],
+    color && !styles[color] && { color },
+    style,
+  ];
+
+  return (
+    <RNText style={textStyles} {...rest}>
+      {children}
+    </RNText>
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: theme.sizes.font,
+    color: theme.colors.black,
+  },
+  regular: {
+    fontWeight: "normal",
+  },
+  bold: {
+    fontWeight: "bold",
+  },
+  semibold: {
+    fontWeight: "600",
+  },
+  medium: {
+    fontWeight: "500",
+  },
+  light: {
+    fontWeight: "200",
+  },
+  center: {
+    textAlign: "center",
+  },
+  right: {
+    textAlign: "right",
+  },
+  h1: theme.fonts.h1,
+  h2: theme.fonts.h2,
+  h3: theme.fonts.h3,
+  title: theme.fonts.title,
+  body: theme.fonts.body,
+  caption: theme.fonts.caption,
+  small: theme.fonts.small,
+});
--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,26 +1,27 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { HeaderButton, Text } from '@react-navigation/elements';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { HeaderButton, Text } from "@react-navigation/elements";
 import {
   createStaticNavigation,
   StaticParamList,
-} from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import AntDesign from '@expo/vector-icons/AntDesign';
-import Feather from '@expo/vector-icons/Feather';
-import { Home } from './screens/Home';
-import { Profile } from './screens/Profile';
-import { Settings } from './screens/Settings';
-import { Updates } from './screens/Updates';
-import { NotFound } from './screens/NotFound';
-import Home2 from './screens/Home2';
-import MovieDetail from './screens/MovieDetail';
+} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import Feather from "@expo/vector-icons/Feather";
+import { Home } from "./screens/Home";
+import { Profile } from "./screens/Profile";
+import { Settings } from "./screens/Settings";
+import { Updates } from "./screens/Updates";
+import { NotFound } from "./screens/NotFound";
+import Home2 from "./screens/Home2";
+import MovieDetail from "./screens/MovieDetail";
+import User from "./screens/User";
 
 const HomeTabs = createBottomTabNavigator({
   screens: {
     Home1: {
       screen: Home,
       options: {
-        title: 'Home1',
+        title: "Home1",
         tabBarIcon: ({ color, size }) => (
           <AntDesign name="book" size={size} color={color} />
         ),
@@ -29,12 +30,12 @@ const HomeTabs = createBottomTabNavigator({
     Home2: {
       screen: Home2,
       options: {
-        title: 'Home2',
+        title: "Home2",
         tabBarIcon: ({ color, size }) => (
           <AntDesign name="setting" size={size} color={color} />
         ),
         tabBarLabelStyle: {
-          fontFamily: 'NotoSansKR',
+          fontFamily: "NotoSansKR",
           fontSize: 10,
         },
       },
@@ -42,7 +43,16 @@ const HomeTabs = createBottomTabNavigator({
     Updates: {
       screen: Updates,
       options: {
-        title: 'Updates',
+        title: "Updates",
+        tabBarIcon: ({ color, size }) => (
+          <Feather name="alert-triangle" size={size} color={color} />
+        ),
+      },
+    },
+    User: {
+      screen: User,
+      options: {
+        title: "User",
         tabBarIcon: ({ color, size }) => (
           <Feather name="alert-triangle" size={size} color={color} />
         ),
@@ -56,29 +66,29 @@ const RootStack = createNativeStackNavigator({
     HomeTabs: {
       screen: HomeTabs,
       options: {
-        title: 'Home',
+        title: "Home",
         headerShown: false,
       },
     },
     MovieDetail: {
       screen: MovieDetail,
       linking: {
-        path: 'movie/:id',
+        path: "movie/:id",
         parse: {
           id: (value) => parseInt(value, 10),
         },
       },
       options: {
-        title: 'Movie Detail',
+        title: "Movie Detail",
         headerShown: true,
       },
     },
     Profile: {
       screen: Profile,
       linking: {
-        path: ':user(@[a-zA-Z0-9-_]+)',
+        path: ":user(@[a-zA-Z0-9-_]+)",
         parse: {
-          user: (value) => value.replace(/^@/, ''),
+          user: (value) => value.replace(/^@/, ""),
         },
         stringify: {
           user: (value) => `@${value}`,
@@ -88,7 +98,7 @@ const RootStack = createNativeStackNavigator({
     Settings: {
       screen: Settings,
       options: ({ navigation }) => ({
-        presentation: 'modal',
+        presentation: "modal",
         headerRight: () => (
           <HeaderButton onPress={navigation.goBack}>
             <Text>Close</Text>
@@ -99,10 +109,10 @@ const RootStack = createNativeStackNavigator({
     NotFound: {
       screen: NotFound,
       options: {
-        title: '404',
+        title: "404",
       },
       linking: {
-        path: '*',
+        path: "*",
       },
     },
   },
